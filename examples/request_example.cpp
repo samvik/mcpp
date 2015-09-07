1f97effb32fd78379d2ef9594000973662e72fa9
Björn Samvik <bjorn@samvik.se>
2015-09-07 20:42:52 +0200

    Added simple example of how to handle a request.

---
+++ examples/request_example.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <sstream>
+
+#include <mcpp/connection.h>
+
+int main(int, char **) {
+
+	mcpp::Connection connection("12345", 
+															"tcp://127.0.0.1:9999", 
+															"tcp://127.0.0.1:9998");
+	
+	int count = 0;
+	
+	std::cout << "Waiting for requests..." << std::endl;
+	for(;;) {
+
+		// Wait for a request
+		mcpp::Request req = connection.recv();
+		
+		std::cout << "Got a " << req.method() << " request for " << req.path() << std::endl;
+
+		// Prepare and send response.
+		if(req.method() == "GET") {
+			std::ostringstream stream;
+			stream << "Hello cruel world #" << ++count << "!";
+
+			connection.replyHttp(req, stream.str());
+		}
+		else {
+			connection.replyHttp(req, mcpp::http::StatusCode::METHOD_NOT_ALLOWED);
+		}
+	}
+	
+}
